4c465e493c8614693c075711b1a244ec88d076eb
torenrl <3530623+torenrl@users.noreply.github.com>
2021-01-08 16:02:21 +0200

    initial file structure

---
+++ Segbot.ino
@@ -0,0 +1,10 @@
+#include <ArduinoBLE>
+#include "pid.h"
+
+void setup() {
+
+}
+
+void loop() {
+    
+}
---
+++ pid.h
@@ -0,0 +1,12 @@
+#ifndef PID_H
+#define PID_H
+
+class PID {
+
+private:
+
+public:
+
+};
+
+#endif //PID_H
